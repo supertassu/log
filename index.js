619871ffb9d90c3ab5583c897c39d674376e75a1
Tassu <git@tassu.me>
2018-06-15 14:24:35 +0300

    stuff! woot
    Signed-off-by: Tassu <git@tassu.me>

--- index.js
+++ index.js
@@ -1,25 +1,79 @@
 import figures from 'figures';
-
-const defaultPattern = [
-  [
-    {
-      text:() => new Date().toLocaleTimeString(),
-      styles: 'grey'
-    },
-    {
-      text = figures.pointerSmall,
-      styles: 'grey'
-    },
-  ]
-];
+import chalk from 'chalk';
 
 const defaultLoggers = {
-  info: Object.assign({}, defaultPattern[0], {
-    text: 'info',
-    styles: 'blue underline'
-  })
+	info: {
+		styling: [
+			{
+				text: () => `[${new Date().toLocaleTimeString()}]`,
+				styles: 'grey'
+			},
+			{
+				text: 'info',
+				styles: ['blue', 'underline']
+			},
+			{
+				text: figures.pointerSmall,
+				styles: 'grey'
+			}
+		],
+		enabled: true
+	}
 };
 
 export default class Logger {
-  output = console.log;
+	output = console.log;
+
+	loggers = undefined;
+
+	constructor() {
+		this.setLoggers(defaultLoggers);
+	}
+
+	_createMessage(objectIn) {
+		let string = '';
+
+		objectIn.forEach(object => {
+			let chalkInstance = chalk;
+
+			if (typeof object.styles === 'string') {
+				chalkInstance = chalkInstance[object.styles];
+			} else if (typeof object.styles === 'object') {
+				object.styles.forEach(element => {
+					chalkInstance = chalkInstance[element];
+				});
+			} else {
+				throw new TypeError('INVALID_TYPE ' + typeof object.styles);
+			}
+
+			if (typeof object.text === 'string') {
+				string += chalkInstance(object.text) + ' ';
+			} else {
+				console.log(JSON.stringify(object));
+				string += chalkInstance(object.text()) + ' ';
+			}
+		});
+
+		return string;
+	}
+
+	setLoggers(loggers) {
+		this.loggers = loggers;
+	}
+
+	log(loggerName, messageIn) {
+		if (!this.loggers[loggerName]) {
+			throw new Error('LOGGER_NOT_FOUND');
+		}
+
+		const logger = this.loggers[loggerName];
+
+		console.log('logger:', JSON.stringify(logger));
+
+		let string = '';
+
+		string += this._createMessage(logger.styling);
+
+		console.log(string);
+	}
 }
---
+++ test.js
@@ -0,0 +1,5 @@
+import Logger from '.';
+
+const logger = new Logger();
+
+logger.log('info', 'hello');
