df3ef601e5a1d870b01708a5d712d726822b70f3
Tassu <git@tassu.me>
2018-06-17 18:32:23 +0300

    tweaks here and there
    Signed-off-by: Tassu <git@tassu.me>

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 import test from 'ava';
-import Logger from '.';
+import Logger from './src';
 
 test.beforeEach(t => {
 	t.context.log = new Logger();
 	t.context.log.output = () => {};
 });
 
-test('crashes if a renderer is not specified', t => {
+test('fails if a renderer is not specified', t => {
 	const error = t.throws(() => t.context.log.log());
 	t.is(error.message, 'LOGGER_NOT_FOUND');
 });
 
-test('test crashes if invalid logger is specified', t => {
+test('fails if invalid logger is specified', t => {
 	const error = t.throws(() => t.context.log.log('PLEASE_DO_NOT_EXIST', 'lol 132'));
 	t.is(error.message, 'LOGGER_NOT_FOUND');
 });
 
-test('crashes if no string is specified', t => {
+test('fails if no string is specified', t => {
 	const error = t.throws(() => t.context.log.log('info'));
 	t.is(error.message, 'NO_MESSAGE_SPECIFIED');
 });
@@ -53,6 +53,16 @@ test('does not print anything if logger is disabled (2)', t => {
 	t.false(t.context.log.log('disabled', 'ayey'));
 });
 
+test('does print if enable() function returns true', t => {
+	t.context.log.addLogger('testing', {
+		styling: [],
+		enabled: () => true
+	}
+	);
+
+	t.true(t.context.log.log('testing', 'ayey'));
+});
+
 test('does not print anything if logger is globally disabled (1)', t => {
 	t.context.log.output = () => {
 		t.fail('something was printed');
@@ -71,4 +81,10 @@ test('does not print anything if logger is globally disabled (2)', t => {
 	t.context.log.enabled = () => false;
 
 	t.false(t.context.log.log('info', 'ayey'));
+});
+
+test('does print if global enable() function returns true', t => {
+	t.context.log.true = () => true;
+
+	t.true(t.context.log.log('info', 'ayey'));
 });
